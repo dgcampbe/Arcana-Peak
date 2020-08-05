6ad58a70e51cddbda84e1b2cbe9e473fcead1084
Dane <dgcampbe@gmail.com>
2020-08-05 19:26:25 -0400

    Added music
    Further improved code style and fleshed out the character creation a little bit. The downside of using enter to skip is that it messes up the continue prompts.

--- RPG.py
+++ RPG.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python
+"""rpg."""
 import sys
-import time
 import os
+import time
+# import fabulous
+import keyboard
 
-#Global vars
+# Global vars
 chars = {}
 attributes = ["Strength",
               "Dexterity",
@@ -12,39 +16,37 @@ attributes = ["Strength",
               "Will",
               "Favor"]
 
+
 class Character(object):
+    """Represent a character and all of their statisics."""
 
-    def __init__(self, attributes, talents, status):
+    def __init__(self, status, is_npc=False):
 
-        self.name = status["name"]
-        print("Character spawned. Please kindly welcome " +
-              self.name + " into the world!")
-        self.attributes = attributes
         self.status = status
-        self.talents = talents
+        print("Character spawned. Please kindly welcome " +
+              self.status["name"] + " into the world!")
 
     def __str__(self):
-
-        rep = {"attributes": {}}
-        for attribute in attributes:
-            rep["attributes"][attribute] = self.attributes[attribute]
-        rep["talents"] = self.talents
-        rep["status"] = self.status
-
+        """String."""
+        rep = ""
+        for i in self.status:
+            rep += i + ": " + str(self.status[i])
         return str(rep)
 
     @property
     def icon(self):
-
+        """Icon."""
         return "[O]"
 
     def save_to_json(self):
-
+        """Save to JSON."""
         f = open(os.path.join("chars", self.name + ".json"), "w")
         f.write(str(self))
         f.close()
 
+
 class Grid(object):
+    """A grid for the game world."""
 
     def __init__(self, dimensions):
 
@@ -58,7 +60,7 @@ class Grid(object):
             self.squares.append([self.empty, ]*self.dimensions[1])
 
     def display(self):
-
+        """Display."""
         rep = "  "
 
         for j in range(self.dimensions[1]):
@@ -72,121 +74,207 @@ class Grid(object):
         print(rep)
 
     def move(self, char, loc):
-
+        """Move."""
         if char in self.entities:
-
             self.squares[self.entities[char][0]][self.entities[char][0]] = self.empty
-
         self.squares[loc[0]][loc[1]] = char.icon
         self.entities[char] = loc
 
+
 def clear(confirm=True):
-
+    """Clear the console window after an optional user confirmation."""
     if confirm:
-        input("Press enter to Continue.")
-
+        input("\nPress enter to Continue.")
     if os.name == 'nt':
         _ = os.system('cls')
-
     else:
         _ = os.system('clear')
 
-def char_from_json(file):
 
+def print_slow(text, rest=0.05, new_line=True, is_input=False, skippable=True):
+    """Slowly print text across the screen."""
+    i = 1
+    for letter in text:
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        if skippable and keyboard.is_pressed("enter"):
+            sys.stdout.write(text[i:])
+            break
+        i += 1
+        time.sleep(rest)
+    if is_input:
+        return input()
+    if new_line:
+        print()
+
+
+def char_from_json(file):
+    """Import a character from JSON."""
     f = open(file)
     char = eval(f.read())
     f.close()
     return Character(char["attributes"], char["talents"], char["status"])
 
+
 def end():
+    """End the game."""
     print("Game terminated.")
     clear()
     sys.exit()
 
+
 def inventory():
+    """Check inventory."""
     print("Inventory does not work yet.")
 
+
 def move(tokens, active_grid):
+    """Move."""
     if tokens[1] in chars:
         active_grid.move(chars[tokens[1]], eval(tokens[2]))
     else:
         print("Sorry, that character does not exist.")
 
-def interpret(text, active_grid):
 
+def interpret(text, active_grid):
+    """Interpret the commands input by the user."""
     tokens = text.split(" ")
-    commands = {"move": lambda: move(tokens, active_grid), "inventory": lambda: inventory(), "end": lambda: end()}
+    commands = {"move": lambda: move(tokens, active_grid),
+                "inventory": lambda: inventory(),
+                "end": lambda: end()}
     if tokens[0] in commands.keys():
         commands[tokens[0]]()
     else:
         print("Your command was not understood. Please check your syntax.")
 
-def char_attributes():
 
+def char_attributes():
+    """Character attributes."""
     user_attributes = {}
-    clear(confirm=False)
-    print("""Welcome to character creation. Here you can assign your attributes.
-Your attributes are strength, dexterity, constitution, intelligence, wisdom, will, and finally favor.
-You have 70 points to distrobute among them. Please choose wisely.""")
+    print("It is time to assign your attributes.")
+    print("Your attributes are strength, dexterity, constitution, "
+          "intelligence, wisdom, will, and finally favor.")
+    print("These will govern how your character "
+          "performs at a variety of tasks.")
+    print("You have 70 points to distribute among them.\n")
+    time.sleep(5)
+    print_slow("Please choose wisely.")
     for attribute in attributes:
         while True:
             try:
-                user_attributes[attribute] = int(input("Your character will have this much " + attribute + ": "))
+                user_attributes[attribute] = int(input("\nYour character will have this much " + attribute + ": "))
                 break
-            except:
+            except ValueError:
                 print("Each attribute must be an integer. Please try again.")
     clear()
     display_attributes(user_attributes)
     return user_attributes
 
+
 def display_attributes(attributes):
+    """Display attributes."""
     for attribute in attributes:
         print("You have " + str(attributes[attribute]) + " " + attribute + ".")
+        time.sleep(0.1)
+
 
 def char_name():
+    """Character name."""
     clear(confirm=False)
     name = input("Now I must ask, who are you? ")
     return name
 
-def char_creation():
 
-    confirm_message = [lambda: "Before we proceed, please confirm if this is correct. Is it? ",
+def snarky_quit(message):
+    """Quit the game unpon too many failed attempts to create a character."""
+    clear(confirm=False)
+    print_slow(message[:15] + "...\n")
+    time.sleep(3)
+    print_slow("You know what?")
+    time.sleep(1)
+    # actually I must have all day
+    # since I spent time programming this message to work
+    print_slow("I don't have all day.")
+    time.sleep(1)
+    print_slow("Maybe this game just isn't for you.")
+    time.sleep(5)
+    end()
+
+
+def char_creation():
+    """Character creation."""
+    confirm_message = [lambda: "Before we proceed, "
+                       "please confirm if this is correct. Is it? ",
                        lambda: "So you are " + name + "? "]
-    fail_message = ["That was a yes or no question, silly! I will ask you again.",
-                    "I am just asking whether that is your name! It isn't that hard of a question. A mere yes or no will suffice."]
+    fail_message = ["That was a yes or no question, silly! "
+                    "I will ask you again.",
+                    "I am just asking whether that is your name! "
+                    "It isn't that hard of a question. "
+                    "A mere yes or no will suffice."]
+    clear(confirm=False)
+    print_slow("Welcome to character creation.\n")
+    time.sleep(1)
     user_attributes = {}
     for i in range(2):
         confirm = "no"
-        while confirm == "no":
+        j = 0
+        while confirm.lower() != "yes":
+            if j == 10:
+                snarky_quit(fail_message[i])
             if i == 0:
                 user_attributes = char_attributes()
-            else:
+            elif i == 1:
                 name = char_name()
             confirm = input(confirm_message[i]())
             while confirm.lower() not in ("yes", "no"):
                 clear(confirm=False)
-                print(fail_message[i])
+                j += 1
+                if j == 10:
+                    snarky_quit(fail_message[i])
+                # provide a snarky message
+                # informing the user about their failure
+                print_slow(fail_message[i])
                 if i == 0:
                     display_attributes(user_attributes)
-                confirm = input(confirm_message[i]())
+                confirm = print_slow(confirm_message[i](), is_input=True)
+            j += 1
     clear(confirm=False)
-    return Character(user_attributes, [], {"name":name, "xp":0, "hp":100, "inventory":[], "alignment":None, "class":None, "race":None})
+    # return the character the player created
+    return Character({"name":  name,
+                      "attributes": user_attributes,
+                      "xp": None,
+                      "hp": None,
+                      "inventory": [],
+                      "alignment": None,
+                      "class": None,
+                      "race": None,
+                      "talents": []})
+
 
 def main():
-
-    print("Welcome to this text-based RPG written in Python.")
+    """Main."""
+    clear(confirm=False)
+    # introduce the game, its creator, and licenses
+    print_slow("Welcome to this text-based "
+               "RPG written in Python.\n", rest=0.1)
+    print_slow("rpg code is licensed under GNU GPL v3.")
+    print_slow("rpg Soundtrack by Dane Campbell "
+               "is licensed under CC BY-NC-SA 4.0.")
+    time.sleep(1)
+    clear()
+    print_slow("Imagine a world...")
     clear()
     player = char_creation()
-    chars[player.name] = player
+    chars[player.status["name"]] = player
     player.save_to_json()
     clear()
     grid1 = Grid([10, 10])
     grid1.display()
-
     while True:
         interpret(input("Please enter your command: "), grid1)
         clear()
         grid1.display()
     clear()
+
 
 main()
--- readme.md
+++ readme.md
@@ -1,49 +1,13 @@
-https://www.dandwiki.com/wiki/5e_Magic_Items_by_Rarity
-https://www.dndbeyond.com/homebrew
-https://en.wikipedia.org/wiki/Magic_of_Dungeons_%26a_Dragons
-https://opengameart.org/
-German RPGs have a powerful and obvious spell for figuring out the density of one's foot. Fuß Rho, Duh
+# A simple text-based rpg in Python. 
+The game will be renamed later.
+## Licensing
+rpg code is licensed under the [GNU GPL v3](https://www.gnu.org/licenses/quick-guide-gplv3.html).
+rpg Soundtrack by Dane Campbell is licensed under [CC BY-NC-SA 4.0](https://creativecommons.org/licenses/by-nc-sa/4.0).
 
-Talents:
-
-Teacher - You create an aura granting +1 Int to nearby allies. (Int > 17)
-Diplomat - You reduce hostility of others. (Speech > 85)
-Student - You earn extra XP. (Int > 13)
-Meat Shield - You redirect 15% of damage dealt to allies. (Con > 17)
-Glass Canon - You have +50% incoming and outgoing damage. (Int > 17)
-Berserker - You deal more damage at lower health. (Con > 17)
-Vampire - You heal for 5% of all damage you deal. (Con > 17)
-Linguist - Your understanding of languages increases. (Int > 13, Speech > 75)
-Doctor - You create an aura that heals HP to nearby allies. (Thinking > 45, Wil > 13)
-
-Attributes:
-Body Attributes:
-Strength (Str) - ability to poccess
-Dexterity (Dex) - ability to move
-Constitution (Con) - ability to resist death
-Mind Attributes:
-Intelligence (Int) - ability to know
-Wisdom (Wis) - ability to reason
-Will (Wil) - ability to change
-Divine Attribute:
-Favor (Fav) - standing with the powers that be
-
-Skills:
-
-Crafting = (3 * Dex) + Int + Wil
-Speech = (3 * Wil) + Wis + Int
-Spirit = Con + Wil  + Wis
-Endurance = (2 * Con) + Str
-Armor = Con + Str + Dex
-Perception = Wis + Wil
-Thinking = (2 * Wis) + Int + Wil
-Divinity = (3 * Fav) + Wis + Wil
-Technology = (3 * Int) + Wis + Wil
-
-Classes:
-
-
-Status:
-
-Health
-XP
+## Dependencies
+Requires:
+* WildMIDI
+* Python 3
+* Python Packages:
+	* fabulous
+	* keyboard
